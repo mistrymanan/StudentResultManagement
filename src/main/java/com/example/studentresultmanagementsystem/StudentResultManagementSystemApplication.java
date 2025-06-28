9b47a18a4b59845f5a86b5cf250e26e8a90a297d
MANN MISTRY <mn732058@dal.ca>
2025-06-28 10:14:34 -0400

    Project init

---
+++ src/main/java/com/example/studentresultmanagementsystem/StudentResultManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentresultmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentResultManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentResultManagementSystemApplication.class, args);
+    }
+
+}
